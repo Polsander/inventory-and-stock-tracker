b5de7c3c6db2b2094e24d813be1e239d9bfebbd7
Oliver <gorillaman2234@gmail.com>
2021-06-25 13:32:26 -0700

    modal fix

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -7,22 +7,22 @@ function modalValue() {
         const field = document.querySelector("input#langley.form-control");
         //field.value captures the numerical value the user inputted
 
-        if(url[2] === "in" && url[3] === "cabinets") {
+        if(url[1] === "in" && url[2] === "cabinets") {
             const cabinetName = document.querySelector("#cabinetName").value
             document.querySelector(".modal-body").innerHTML = `Are you sure you want to take <strong> IN </strong> ${field.value} ${cabinetName} cabinets?`
         }
 
-        if(url[2] === "in" && url[3] === "units") {
+        if(url[1] === "in" && url[2] === "units") {
             const unitName = document.querySelector("#unitName").value
             document.querySelector(".modal-body").innerHTML = `Are you sure you want to take <strong> IN </strong> ${field.value} ${unitName} units?`
         }
 
-        if(url[2] === "out" && url[3] === "cabinets") {
+        if(url[1] === "out" && url[2] === "cabinets") {
             const cabinetName = document.querySelector("#cabinetName").value
             document.querySelector(".modal-body").innerHTML = `Are you sure you want to send <strong> OUT </strong> ${field.value} ${cabinetName} cabinets?`
         }
 
-        if(url[2] === "out" && url[3] === "units") {
+        if(url[1] === "out" && url[2] === "units") {
             const unitName = document.querySelector("#unitName").value
             document.querySelector(".modal-body").innerHTML = `Are you sure you want to take <strong> OUT </strong> ${field.value} ${unitName} units?`
         }
